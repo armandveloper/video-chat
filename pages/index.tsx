412002b54d4f883a5a21d2ed449c3288def9f358
Armando <jaimes087.uz@gmail.com>
2021-07-14 09:54:40 -0500

    Complete the ui of the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,49 @@ const Header = styled.header`
 `;
 
 const Logo = styled.h1`
-	font-size: 2.6rem;
+	font-size: 2rem;
 	font-weight: 400;
 	letter-spacing: 0.1rem;
+	@media (min-width: 48em) {
+		font-size: 2.6rem;
+	}
 `;
 
 const Container = styled.main`
-	border: 1px solid red;
-	max-width: 50rem;
+	max-width: 62rem;
 	margin: 4rem auto 0;
+	padding: 0 1rem;
+`;
+
+const Headline = styled.h2`
+	font-size: 1.8rem;
+	font-weight: 500;
+	margin: 0 0 4rem;
+	text-align: center;
+	@media (min-width: 48em) {
+		font-size: 2.5rem;
+	}
+`;
+
+const InlineGrid = styled.div`
+	max-width: 50rem;
+	margin: auto;
 	display: grid;
 	grid-template-columns: 2fr 3fr 1fr;
 	gap: 2rem;
-	align-items: start;
+	${Button}:first-child {
+		grid-column: 1 / -1;
+	}
+	@media (min-width: 48em) {
+		${Button}:first-child {
+			grid-column: 1 / 2;
+		}
+	}
 `;
 
 const InputWrapper = styled.div`
+	grid-row: 2;
+	grid-column: 1 / 3;
 	border: 1px solid transparent;
 	padding: 0.6rem 1rem;
 	transition: 0.15s ease;
@@ -39,9 +66,14 @@ const InputWrapper = styled.div`
 	> * {
 		flex-shrink: 0;
 	}
+	@media (min-width: 48em) {
+		grid-row: 1;
+		grid-column: 2;
+	}
 `;
 
 const Input = styled.input`
+	grid-row: 2;
 	flex: 1;
 	background: none;
 	border: none;
@@ -51,6 +83,11 @@ const Input = styled.input`
 	font-size: 1.4rem;
 	&:focus {
 		outline: none;
+	}
+	::placeholder {
+		color: var(--color-text);
+		font: inherit;
+		opacity: 0.79;
 	}
 `;
 
@@ -62,15 +99,29 @@ function HomePage() {
 				<Button>Login</Button>
 			</Header>
 			<Container>
-				<Button>
-					<MdVideoCall size="24" color="currentColor" />
-					New Meeting
-				</Button>
-				<InputWrapper>
-					<MdKeyboard size="24" color="currentColor" opacity="0.79" />
-					<Input type="text" />
-				</InputWrapper>
-				<Button>Join</Button>
+				<Headline>
+					Create a new meeting or enter a code or link to join an
+					existing one.
+				</Headline>
+				<InlineGrid>
+					<Button>
+						<MdVideoCall size="24" color="currentColor" />
+						New Meeting
+					</Button>
+					<InputWrapper>
+						<MdKeyboard
+							size="24"
+							color="currentColor"
+							opacity="0.79"
+						/>
+						<Input
+							type="text"
+							aria-label="Enter a code or link"
+							placeholder="Enter a code or link"
+						/>
+					</InputWrapper>
+					<Button>Join</Button>
+				</InlineGrid>
 			</Container>
 		</Layout>
 	);
